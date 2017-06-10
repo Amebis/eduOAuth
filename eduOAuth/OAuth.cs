396c165669cbb9de7876b6e39ea669113b755979
Simon Rozman <simon@rozman.si>
2017-06-10 13:54:43 +0200

    Initial commit

---
+++ eduOAuth/OAuth.cs
@@ -0,0 +1,13 @@
+﻿/*
+    eduOAuth - An OAuth 2.0 Client for eduVPN (and beyond)
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+namespace eduOAuth
+{
+    public class OAuth
+    {
+    }
+}
