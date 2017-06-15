91a21a6e86272349c473cb23883aafef17ab1be5
Simon Rozman <simon@rozman.si>
2017-06-15 13:51:50 +0200

    Source annotation

--- eduOAuth/AuthorizationGrant.cs
+++ eduOAuth/AuthorizationGrant.cs
@@ -12,7 +12,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net;
 using System.Security;
-//using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -20,6 +19,140 @@ namespace eduOAuth
 {
     class AuthorizationGrant
     {
+        #region Data Types
+
+        /// <summary>
+        /// Code challenge algorithm method types
+        /// </summary>
+        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.2"/>
+        public enum CodeChallengeAlgorithmType
+        {
+            /// <summary>
+            /// PKCE disabled
+            /// </summary>
+            None,
+
+            /// <summary>
+            /// Code challenge = Code verifier
+            /// </summary>
+            Plain,
+
+            /// <summary>
+            /// Code challenge = Base64URLEncodeNoPadding(SHA256(ASCII(Code verifier)))
+            /// </summary>
+            S256,
+        }
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// Random client state
+        /// </summary>
+        private string state;
+
+        /// <summary>
+        /// PKCE code verifier
+        /// </summary>
+        private string code_verifier;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Authorization endpoint base URI
+        /// </summary>
+        public Uri AuthorizationEndpoint { get; set; }
+
+        /// <summary>
+        /// Redirection endpoint base URI
+        /// </summary>
+        /// <remarks>Client should setup a listener on this URI prior this method is called.</remarks>
+        public Uri RedirectEndpoint { get; set; }
+
+        /// <summary>
+        /// Client ID
+        /// </summary>
+        /// <remarks>Should be populated before requesting authorization.</remarks>
+        public string ClientID { get; set; }
+
+        /// <summary>
+        /// Client secret or <c>null</c> if client is not issued credentials.
+        /// </summary>
+        public SecureString ClientSecret { get; set; }
+
+        /// <summary>
+        /// Code challenge algorithm method
+        /// </summary>
+        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.2"/>
+        public CodeChallengeAlgorithmType CodeChallengeAlgorithm { get; set; }
+
+        /// <summary>
+        /// List of scope identifiers client is requesting access
+        /// </summary>
+        /// <remarks>Should be populated before requesting authorization. When empty <c>scope</c> parameter is not included in authorization request URI.</remarks>
+        public List<string> Scope { get; set; }
+
+        /// <summary>
+        /// Authorization URI
+        /// </summary>
+        /// <see cref="https://tools.ietf.org/html/rfc6749#section-4.1.1"/>
+        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.1"/>
+        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.2"/>
+        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.3"/>
+        public Uri AuthorizationURI
+        {
+            get
+            {
+                // Prepare authorization endpoint URI.
+                var auth_uri_builder = new UriBuilder(AuthorizationEndpoint);
+                auth_uri_builder.Query +=
+                    (auth_uri_builder.Query.Length > 0 ? "&" : "?") + "response_type=code" +
+                    "&client_id=" + Uri.EscapeDataString(ClientID) +
+                    "&redirect_uri=" + Uri.EscapeDataString(RedirectEndpoint.ToString());
+
+                if (Scope.Count > 0)
+                {
+                    // Add the client requested scope.
+                    auth_uri_builder.Query += "&scope=" + Uri.EscapeDataString(String.Join(" ", Scope.ToArray()));
+                }
+
+                // Add the random state.
+                auth_uri_builder.Query += "&state=" + state;
+
+                if (CodeChallengeAlgorithm != CodeChallengeAlgorithmType.None)
+                {
+                    // Add the code challenge (RFC 7636).
+                    switch (CodeChallengeAlgorithm)
+                    {
+                        case CodeChallengeAlgorithmType.Plain:
+                            auth_uri_builder.Query += "&code_challenge_method=plain&code_challenge=" + code_verifier;
+                            break;
+
+                        case CodeChallengeAlgorithmType.S256:
+                            {
+                                var sha256 = new SHA256Managed();
+                                auth_uri_builder.Query += "&code_challenge_method=S256&code_challenge=" + Base64URLEncodeNoPadding(sha256.ComputeHash(Encoding.ASCII.GetBytes(code_verifier)));
+                            }
+                            break;
+                    }
+                }
+                else
+                    code_verifier = null;
+
+                //// Opens request in the browser.
+                //System.Diagnostics.Process.Start(auth_uri_builder.Uri.ToString());
+
+                return auth_uri_builder.Uri;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
         /// <summary>
         /// Initializes default authorization grant.
         /// </summary>
@@ -38,6 +171,10 @@ namespace eduOAuth
             rng.GetBytes(random);
             code_verifier = Base64URLEncodeNoPadding(random);
         }
+
+        #endregion
+
+        #region Methods
 
         /// <summary>
         /// Parses authorization grant received and requests access token if successful.
@@ -140,147 +277,6 @@ namespace eduOAuth
             return s;
         }
 
-        //// ref http://stackoverflow.com/a/3978040
-        //protected static int GetRandomUnusedPort()
-        //{
-        //    var listener = new TcpListener(IPAddress.Loopback, 0);
-        //    listener.Start();
-        //    var port = ((IPEndPoint)listener.LocalEndpoint).Port;
-        //    listener.Stop();
-        //    return port;
-        //}
-
-        /// <summary>
-        /// Authorization endpoint base URI
-        /// </summary>
-        public Uri AuthorizationEndpoint { get; set; }
-
-        /// <summary>
-        /// Redirection endpoint base URI
-        /// </summary>
-        /// <remarks>Client should setup a listener on this URI prior this method is called.</remarks>
-        public Uri RedirectEndpoint { get; set; }
-
-        /// <summary>
-        /// Client ID
-        /// </summary>
-        /// <remarks>Should be populated before requesting authorization.</remarks>
-        public string ClientID { get; set; }
-
-        /// <summary>
-        /// Client secret or <c>null</c> if client is not issued credentials.
-        /// </summary>
-        public SecureString ClientSecret { get; set; }
-
-        /// <summary>
-        /// Code challenge algorithm method types
-        /// </summary>
-        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.2"/>
-        public enum CodeChallengeAlgorithmType
-        {
-            /// <summary>
-            /// PKCE disabled
-            /// </summary>
-            None,
-
-            /// <summary>
-            /// Code challenge = Code verifier
-            /// </summary>
-            Plain,
-
-            /// <summary>
-            /// Code challenge = Base64URLEncodeNoPadding(SHA256(ASCII(Code verifier)))
-            /// </summary>
-            S256,
-        }
-
-        /// <summary>
-        /// Code challenge algorithm method
-        /// </summary>
-        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.2"/>
-        public CodeChallengeAlgorithmType CodeChallengeAlgorithm { get; set; }
-
-        /// <summary>
-        /// List of scope identifiers client is requesting access
-        /// </summary>
-        /// <remarks>Should be populated before requesting authorization. When empty <c>scope</c> parameter is not included in authorization request URI.</remarks>
-        public List<string> Scope { get; set; }
-
-        /// <summary>
-        /// Authorization URI
-        /// </summary>
-        /// <see cref="https://tools.ietf.org/html/rfc6749#section-4.1.1"/>
-        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.1"/>
-        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.2"/>
-        /// <see cref="https://tools.ietf.org/html/rfc7636#section-4.3"/>
-        public Uri AuthorizationURI
-        {
-            get
-            {
-                //// Create a redirect URI using an available port on the loopback address.
-                //var redirect_uri = "http://" + IPAddress.Loopback + ":" + GetRandomUnusedPort()  + "/callback";
-
-                //// Start a HttpListener call-back (ASAP, while the TCP port is still free).
-                //http = new HttpListener();
-                //http.Prefixes.Add(redirect_uri);
-                //http.Start();
-
-                // Prepare authorization endpoint URI.
-                var auth_uri_builder = new UriBuilder(AuthorizationEndpoint);
-                auth_uri_builder.Query +=
-                    (auth_uri_builder.Query.Length > 0 ? "&" : "?") + "response_type=code" +
-                    "&client_id=" + Uri.EscapeDataString(ClientID) +
-                    "&redirect_uri=" + Uri.EscapeDataString(RedirectEndpoint.ToString());
-
-                if (Scope.Count > 0)
-                {
-                    // Add the client requested scope.
-                    auth_uri_builder.Query += "&scope=" + Uri.EscapeDataString(String.Join(" ", Scope.ToArray()));
-                }
-
-                // Add the random state.
-                auth_uri_builder.Query += "&state=" + state;
-
-                if (CodeChallengeAlgorithm != CodeChallengeAlgorithmType.None)
-                {
-                    // Add the code challenge (RFC 7636).
-                    switch (CodeChallengeAlgorithm)
-                    {
-                        case CodeChallengeAlgorithmType.Plain:
-                            auth_uri_builder.Query += "&code_challenge_method=plain&code_challenge=" + code_verifier;
-                            break;
-
-                        case CodeChallengeAlgorithmType.S256:
-                            {
-                                var sha256 = new SHA256Managed();
-                                auth_uri_builder.Query += "&code_challenge_method=S256&code_challenge=" + Base64URLEncodeNoPadding(sha256.ComputeHash(Encoding.ASCII.GetBytes(code_verifier)));
-                            }
-                            break;
-                    }
-                }
-                else
-                    code_verifier = null;
-
-                //// Opens request in the browser.
-                //System.Diagnostics.Process.Start(auth_uri_builder.Uri.ToString());
-
-                return auth_uri_builder.Uri;
-            }
-        }
-
-        /// <summary>
-        /// Random client state
-        /// </summary>
-        private string state;
-
-        /// <summary>
-        /// PKCE code verifier
-        /// </summary>
-        private string code_verifier;
-
-        ///// <summary>
-        ///// HTTP server waiting for a call-back
-        ///// </summary>
-        //private HttpListener http;
+        #endregion
     }
 }
