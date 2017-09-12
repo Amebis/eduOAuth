a2ee359d16c557142945e5ace62237d3ed336858
Simon Rozman <simon@rozman.si>
2017-09-12 14:37:24 +0200

    Manufacturer update

--- eduOAuth/Properties/AssemblyInfo.cs
+++ eduOAuth/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OAuth 2.0 Library")]
-[assembly: AssemblyCompany("SURFnet")]
+[assembly: AssemblyCompany("SURF")]
 [assembly: AssemblyProduct("eduOAuth")]
 [assembly: AssemblyCopyright("Copyright © The Commons Conservancy eduVPN Programme 2017")]
 [assembly: NeutralResourcesLanguage("en-US")]
