Amebis/eduOAuth

commit 768564e45f401d07515a6591a2af2cf9f30ec2cf

Use HashSet<string> for OAuth scopes
List<> and string[] are not optimal data structures for OAuth scope
sets.

eduOAuth/AccessToken.cs
@@ -62,8 +62,8 @@ namespace eduOAuth
         /// <summary>
         /// List of access token scope identifiers
         /// </summary>
-        public string[] Scope { get => _scope; }
-        private string[] _scope;
+        public HashSet<string> Scope { get => _scope; }
+        private HashSet<string> _scope;
 
         #endregion
 
@@ -83,16 +83,16 @@ namespace eduOAuth
             // Get expiration date.
             Expires = eduJSON.Parser.GetValue(obj, "expires_in", out int expires_in) ? DateTime.Now.AddSeconds(expires_in) : DateTime.MaxValue;
 
-            // Get refresh token
+            // Get refresh token.
             if (eduJSON.Parser.GetValue(obj, "refresh_token", out string refresh_token))
             {
                 _refresh = (new NetworkCredential("", refresh_token)).SecurePassword;
                 _refresh.MakeReadOnly();
             }
 
-            // Get scope
+            // Get scope.
             if (eduJSON.Parser.GetValue(obj, "scope", out string scope))
-                _scope = scope.Split(null);
+                _scope = new HashSet<string>(scope.Split(null));
         }
 
         #endregion
@@ -143,7 +143,7 @@ namespace eduOAuth
         /// <param name="scope">Expected scope</param>
         /// <param name="ct">The token to monitor for cancellation requests</param>
         /// <returns>Access token</returns>
-        public static AccessToken FromAuthorizationServerResponse(HttpWebRequest req, string[] scope = null, CancellationToken ct = default(CancellationToken))
+        public static AccessToken FromAuthorizationServerResponse(HttpWebRequest req, HashSet<string> scope = null, CancellationToken ct = default(CancellationToken))
         {
             var task = FromAuthorizationServerResponseAsync(req, scope, ct);
             try
@@ -165,7 +165,7 @@ namespace eduOAuth
         /// <param name="ct">The token to monitor for cancellation requests</param>
         /// <returns>Asynchronous operation with expected access token</returns>
         [SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Justification = "HttpWebResponse, Stream, and StreamReader tolerate multiple disposes.")]
-        public static async Task<AccessToken> FromAuthorizationServerResponseAsync(HttpWebRequest req, string[] scope = null, CancellationToken ct = default(CancellationToken))
+        public static async Task<AccessToken> FromAuthorizationServerResponseAsync(HttpWebRequest req, HashSet<string> scope = null, CancellationToken ct = default(CancellationToken))
         {
             try
             {
@@ -387,7 +387,7 @@ namespace eduOAuth
 
             // Load other fields and properties.
             Expires = (DateTime)info.GetValue("Expires", typeof(DateTime));
-            _scope = (string[])info.GetValue("Scope", typeof(string[]));
+            _scope = (HashSet<string>)info.GetValue("Scope", typeof(HashSet<string>));
         }
 
         [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]

eduOAuth.Tests/AuthorizationGrantTests.cs
@@ -24,7 +24,7 @@ namespace eduOAuth.Tests
                 RedirectEndpoint = new Uri("org.eduvpn.app:/api/callback"),
                 ClientID = "org.eduvpn.app",
                 CodeChallengeAlgorithm = AuthorizationGrant.CodeChallengeAlgorithmType.S256,
-                Scope = new List<string>() { "scope1", "scope2" },
+                Scope = new HashSet<string>() { "scope1", "scope2" },
             };
 
             var uri_builder = new UriBuilder(ag.AuthorizationURI);

eduOAuth/AuthorizationGrant.cs
@@ -87,7 +87,7 @@ namespace eduOAuth
         /// List of scope identifiers client is requesting access
         /// </summary>
         /// <remarks>Should be populated before requesting authorization. When empty <c>scope</c> parameter is not included in authorization request URI.</remarks>
-        public List<string> Scope { get; set; }
+        public HashSet<string> Scope { get; set; }
 
         /// <summary>
         /// Random client state
@@ -304,7 +304,7 @@ namespace eduOAuth
                 await stream_req.WriteAsync(body_binary, 0, body_binary.Length, ct);
 
                 // Parse response.
-                return await AccessToken.FromAuthorizationServerResponseAsync(request, Scope != null ? Scope.ToArray() : null, ct);
+                return await AccessToken.FromAuthorizationServerResponseAsync(request, Scope, ct);
             }
         }
 

commit cfe19de8bbe41c00cc29d33293396954d7d8987f

Serialize OAuth scopes as string[] again
This commit reverts serialization backward compatibility broken in
768564e45f401d07515a6591a2af2cf9f30ec2cf.

eduOAuth/AccessToken.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
@@ -380,23 +381,19 @@ namespace eduOAuth
             // Load access token.
             _token = Unprotect((byte[])info.GetValue("Token", typeof(byte[])));
 
+            // Load refresh token.
             byte[] refresh = null;
-            try
-            {
-                refresh = (byte[])info.GetValue("Refresh", typeof(byte[]));
-            }
+            try { refresh = (byte[])info.GetValue("Refresh", typeof(byte[])); }
             catch (SerializationException) { }
-            if (refresh != null)
-            {
-                // Load refresh token.
-                _refresh = Unprotect(refresh);
-            }
-            else
-                _refresh = null;
+            _refresh = refresh != null ? Unprotect(refresh) : null;
 
             // Load other fields and properties.
             Expires = (DateTime)info.GetValue("Expires", typeof(DateTime));
-            _scope = (HashSet<string>)info.GetValue("Scope", typeof(HashSet<string>));
+
+            string[] scope = null;
+            try { scope = (string[])info.GetValue("Scope", typeof(string[])); }
+            catch (SerializationException) { }
+            _scope = scope != null ? new HashSet<string>(scope) : null;
         }
 
         [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
@@ -405,15 +402,14 @@ namespace eduOAuth
             // Save access token.
             info.AddValue("Token", Protect(_token));
 
+            // Save refresh token.
             if (_refresh != null)
-            {
-                // Save refresh token.
                 info.AddValue("Refresh", Protect(_refresh));
-            }
 
             // Save other fields and properties.
             info.AddValue("Expires", Expires);
-            info.AddValue("Scope", _scope);
+            if (_scope != null)
+                info.AddValue("Scope", _scope.ToArray());
         }
 
         #endregion